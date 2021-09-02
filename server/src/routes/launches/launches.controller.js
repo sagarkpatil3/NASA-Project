4d867c717eb0a482c6414cb126ca7d7978726f03
Sagar Patil <sagarkp3@gmail.com>
2021-09-02 21:12:03 +0530

    added some test cases for launch controller

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -13,11 +13,12 @@ function httpAddNewLaunch(req, res){
     }
 
     launch.launchDate = new Date(launch.launchDate);
-    if(!isNaN(launch.launchDat)){
+    if (isNaN(launch.launchDate)) {
         return res.status(400).json({
-            error: 'Invalid launch Date'
-        })
+        error: 'Invalid launch date',
+        });
     }
+    
     addNewLaunch(launch)
     return res.status(201).json(launch)
 }
